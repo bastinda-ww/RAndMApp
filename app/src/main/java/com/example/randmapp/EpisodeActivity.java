48e50b9af713522ea34799818f934f8a3272f419
bastinda-ww <andreidenisov96@gmail.com>
2022-03-07 21:33:44 +0400

    add episode activity.

---
+++ app/src/main/java/com/example/randmapp/EpisodeActivity.java
@@ -0,0 +1,15 @@
+package com.example.randmapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EpisodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_epesodes);
+    }
+
+}
